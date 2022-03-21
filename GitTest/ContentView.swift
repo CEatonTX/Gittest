a6cb5a91baffc0f30be32f18ae788a6943453efb
Chris Eaton <36697853+CEatonTX@users.noreply.github.com>
2022-03-21 09:13:49 -0500

    Add files via upload

---
+++ GitTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitTest
+//
+//  Created by Sgt Eaton on 3/21/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitTest/GitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestApp.swift
+//  GitTest
+//
+//  Created by Sgt Eaton on 3/21/22.
+//
+
+import SwiftUI
+
+@main
+struct GitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
